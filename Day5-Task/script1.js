9c47e1c3cdd057edd0373dafde562adf1179a2fd
Dharshni-19 <158270179+Dharshni-19@users.noreply.github.com>
2024-05-27 23:34:31 +0530

    Add files via upload

---
+++ Day5-Task/script1.js
@@ -0,0 +1,91 @@
+{
+    "basics": {
+      "name": "Sree Dharshni Ilango",
+      "email": "sreedharshniilango@gmail.com",
+      "phone": "9514795141",
+      "degree": "B.Tech",
+      "website": "https://dharshni-19.github.io/Portfolio/",
+      "summary": "A Frontend Developer looking for an opportunity to explore in IT sector",
+      "location": {
+        "address": "96/82, ashok nagar, 3rd st, new washermenpet",
+        "postalCode": "600081",
+        "city": "Chennai",
+        "countryCode": "India",
+        "region": "Tamil Nadu"
+      },
+      "profiles": [
+        {
+          "network": "LinkedIn",
+          "username": "Sree Dharshni Ilango",
+          "url": "http://www.linkedin.com/in/sreedharshniilango"
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "Anna University",
+        "area": "B.Tech",
+        "studyType": "Full-Time",
+        "startDate": "14-Aug-2018",
+        "endDate": "22-July-2022",
+        "gpa": "74.9",
+        "courses": [
+          "Ceramic Technology"
+        ]
+      }
+    ],
+    "awards": [
+      {
+        "title": "Your award title",
+        "date": "Your date, in YYYY-MM-DD format you received the award",
+        "awarder": "Your award given by",
+        "summary": "A one-sentence to one-paragraph overview of this award"
+      }
+    ],
+    "skills": [
+      {
+        "name": "Frontend Skills",
+        "level": "Well-known",
+        "keywords": ["HTML, CSS, JavaScript"
+        ]
+      }
+      {
+        "name": "Backend Skills",
+        "level": "Well-known",
+        "keywords": ["Python"
+        ]
+      }
+      {
+        "name": "Devops Tools",
+        "level": "Well-Known",
+        "Keywords": ["Docker", "Ansible", "Jenkins"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil",
+        "fluency": "Mother-Tongue"
+      }
+      {
+        "language": "English",
+        "fluency": "Professional level"
+      }
+      {
+        "language": "Hindi",
+        "fluency": "Read and write"
+      }
+      {
+        "language": "Sanskrit",
+        "fluency": "Read and write"
+      }
+    ],
+    "interests": [
+      {
+        "name": "A category of interests (e.g. 'Sports')",
+        "keywords": [
+          "Keywords under this category (e.g. 'Cricket', 'Football', 'Golf')"
+        ]
+      }
+    ]
+  }
